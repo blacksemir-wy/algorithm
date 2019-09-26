be28af1d8a0b51b7677c4070f3769addacb20a60
yu.wang <wangyu_bit@163.com>
2019-09-26 23:22:45 +0800

    提交代码

---
+++ 啊哈算法/Chapter4/DFS.py
@@ -0,0 +1,25 @@
+def DFS(list):
+    #list为需要全排列的列表
+    step = len(outlist)
+    # 深度为3
+    if step == 3:
+        print(outlist)
+        return outlist
+    else:
+        #flag == 0 means x is on hand
+        for index,value in enumerate(list) :
+            if value not in outlist:
+                outlist.append(value)
+                # print(outlist)
+                DFS(list)
+                #在结束一次递归后返回上一节点，并删除list中的最后一个数
+                #画个3叉树理解下，如何从[1,3,3]跳出到[1]
+                outlist.pop()
+
+                # print(outlist)
+
+list =[1,2,3]
+flag =[0 for x in range(3)]
+outlist = []
+DFS(list)
+
